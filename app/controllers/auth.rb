7f20d0590e2db9fad237ebac1993806fa30f77f5
SputterPuttRedux <dstanfield.esq@gmail.com>
2015-03-20 15:33:52 -0400

    Add logic to auth controller for login. Set session

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -7,6 +7,14 @@ get '/login' do
 end
 
 post '/login' do
+  user = User.find_by(name: params[:user][:name])
+
+  if user.try(:authenticate, params[:user][:password])
+    session[:user_id] = user.user_id
+    redirect '/'
+  else
+    redirect '/login'
+  end
 end
 
 get '/signup' do

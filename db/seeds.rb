882fd89d0e831e91133c047e045cffd63401eb39
SputterPuttRedux <dstanfield.esq@gmail.com>
2015-03-20 16:02:51 -0400

    Add final fake seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
     password_digest: "password"
     )
 end
+
+5.times do |i|
+  rand(10).times do
+    User.find(i + 1).lists.create(
+      complete: [true,false].sample,
+      title: Faker::Lorem.sentence
+      )
+  end
+end
+
+List.all.length.times do |i|
+  rand(8).times do
+    List.find(i + 1).items.create(
+      complete: [true,false].sample,
+      content: Faker::Lorem.word
+      )
+  end
+end
+
+

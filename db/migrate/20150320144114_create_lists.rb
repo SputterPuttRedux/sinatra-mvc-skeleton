04e3bb7531c19f51d76e2f19f1f975bfb984cba9
SputterPuttRedux <dstanfield.esq@gmail.com>
2015-03-20 14:51:25 -0400

    Add fields to createlists migration

--- db/migrate/20150320144114_create_lists.rb
+++ db/migrate/20150320144114_create_lists.rb
@@ -1,4 +1,11 @@
 class CreateLists < ActiveRecord::Migration
   def change
+    create_table :lists do |t|
+      t.string :title
+      t.string :complete, :boolean, default: false
+      t.references :user
+
+      t.timestamps
+    end
   end
 end

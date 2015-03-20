42a7722946f88776384ba30ac3bfa9843a483d75
SputterPuttRedux <dstanfield.esq@gmail.com>
2015-03-20 14:54:56 -0400

    Correct typo create_lists. change field to boolean.  Add fields to create_items

--- db/migrate/20150320144114_create_lists.rb
+++ db/migrate/20150320144114_create_lists.rb
@@ -2,7 +2,7 @@ class CreateLists < ActiveRecord::Migration
   def change
     create_table :lists do |t|
       t.string :title
-      t.string :complete, :boolean, default: false
+      t.boolean :complete, default: false
       t.references :user
 
       t.timestamps
--- db/migrate/20150320144126_create_items.rb
+++ db/migrate/20150320144126_create_items.rb
@@ -1,4 +1,9 @@
 class CreateItems < ActiveRecord::Migration
   def change
+    create_table :items do |t|
+      t.references :list
+      t.string :content
+      t.boolean :complete, default: false
+    end
   end
 end

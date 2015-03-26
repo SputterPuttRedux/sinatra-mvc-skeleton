e7b2834d524bd0f058b4e5633ca6192702dec5aa
SputterPuttRedux <dstanfield.esq@gmail.com>
2015-03-25 20:09:23 -0400

    Create helper methods for note completion status, 'username' maker, and timestamps display

--- app/helpers/auth.rb
+++ app/helpers/auth.rb
@@ -5,3 +5,19 @@ def current_user
     return nil
   end
 end
+
+def temp_username
+  current_user.email.gsub(/@\w+.\w+/, "")
+end
+
+def note_timestamp(note)
+  note.created_at == note.updated_at ? "Created at: #{note.created_at}" : "Updated at: #{note.updated_at}"
+end
+
+def num_notes_complete(notes)
+  notes.select{ |note| note.complete == true }.count
+end
+
+def num_notes_incomplete(notes)
+  notes.select{ |note| note.complete == false }.count
+end

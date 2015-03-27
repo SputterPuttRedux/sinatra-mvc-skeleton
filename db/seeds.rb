fd2c70db9de9320520ebcfaa7413c0d39b47d1df
SputterPuttRedux <dstanfield.esq@gmail.com>
2015-03-27 14:37:45 -0400

    Comment out faker data to deploy to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-5.times do
-  User.create(
-    email: Faker::Internet.email,
-    password: "password",
-    password_confirmation: "password"
-    )
-end
+# 5.times do
+#   User.create(
+#     email: Faker::Internet.email,
+#     password: "password",
+#     password_confirmation: "password"
+#     )
+# end
 
 User.create(
   email: "foo@bar.edu",
@@ -12,14 +12,14 @@ User.create(
   password_confirmation: "password"
   )
 
-6.times do |i|
-  rand(4..10).times do
-    User.find(i + 1).notes.create(
-      complete: [true,false].sample,
-      content: Faker::Lorem.paragraph
-      )
-  end
-end
+# 6.times do |i|
+#   rand(4..10).times do
+#     User.find(i + 1).notes.create(
+#       complete: [true,false].sample,
+#       content: Faker::Lorem.paragraph
+#       )
+#   end
+# end
 
 
 

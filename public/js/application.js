79f707c8a067104e01b22a5fdab916fd17469ae8
SputterPuttRedux <dstanfield.esq@gmail.com>
2015-04-06 16:14:35 -0400

    Write javascript method to display font-awesome appropriate font-awesome icons.  Begin to write AJAX call to update completion status.

--- public/js/application.js
+++ public/js/application.js
@@ -1,12 +1,44 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+function completionIconDisplay (){
+ var $statusArray = $('.current-status');
+ for(i=0; i < $statusArray.length; i++){
+  var $currentCell = $($statusArray[i]);
+  if ($currentCell.html()==="false"){
+    $currentCell.html('<a href="" title="Change Completion Status"><i class="fa fa-times"></i></a>');
+  } else if($currentCell.html()==="true"){
+    $currentCell.html('<a href="" title="Change Completion Status"><i class="fa fa-check"></i></a>');
+  }
+ }
+}
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+// function completionRowColor(){
+//   $noteRow = $('.note-row');
+//   for(i=0; i < $noteRow.length; i++){
+//     if($noteRow.find('i')[i].className === 'fa fa-times'){
+//       $($noteRow[i]).css("background-color", "#FF6464");
+//     } else {
+//       $($noteRow[i]).css("background-color", "#55D955");
+//     }
+//   }
+// }
+
+
+$(document).ready(function() {
 
   $('#back-button').click(function() {
     parent.history.back();
     return false;
   });
+
+  completionIconDisplay();
+  // completionRowColor();
+  var $completionIcon = $('.current-status').children().children('i');
+
+  $completionIcon.on('click', function(event){
+    event.preventDefault();
+    var $target = $(event.target);
+    $.ajax( $target.attr('class', 'fa fa-times'));
+  });
+
+// stopped here. trying to write an ajax call that will execute the helper method I wrote that changes the completion status of the individual note.
+
 });

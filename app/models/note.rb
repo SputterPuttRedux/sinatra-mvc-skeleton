7d4363a9b0aec26227eb56feea4416b01d16d9d5
SputterPuttRedux <dstanfield.esq@gmail.com>
2015-04-06 21:37:54 -0400

    Create change completion status model method

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,12 @@
 class Note < ActiveRecord::Base
   belongs_to :user
+
+  def change_completion_status
+    if self.complete
+      self.update(complete: false)
+    else
+      self.update(complete: true)
+    end
+  end
+
 end
